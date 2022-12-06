502da20588adc0f0dc37a59d22d5d1f7efaeeb8a
BigPoppe <tzelawski@gmail.com>
2022-12-06 15:13:04 +0100

    day 5 done

--- 05/script.sh
+++ 05/script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #making input easy to parse
-tr -d '[]' < input.dat | tr '  ' ' ' | sed -E 's/ *[[:alpha:]]+ *([[:digit:]]+)/\1 /g' | sed -E 's/    / -/g' | sed -E 's/ *([[:digit:]]) +/\1 /g'
+tr -d '[]' | tr '  ' ' ' |
+sed -E 's/ *[[:alpha:]]+ *([[:digit:]]+)/\1 /g' |
+sed -E 's/    / -/g' |
+sed -E 's/-([[:alpha:]])/- \1/g' |
+sed -E 's/^ *//g' |
+sed -E 's/ *([[:digit:]]) +/\1 /g' |
+sed -E 's/ +$//g'
--- 05/solution.py
+++ 05/solution.py
@@ -1,24 +1,76 @@
 import re
-list = []
-line = input()
-#normalizedLine = re.sub("", "[[]]", line)
-normalizedLine = re.sub("    ", " -", line)
-splitLine = normalizedLine.split(" ")
-for s in splitLine:
-    list.append([])
-
-while True:
-    i = 0
+def first_part():
+    list = []
+    line = input()
+    splitLine = line.split(" ")
     for s in splitLine:
-        if(s != "-"):
-            list[i].append(s)
-        i+=1
-    try:
-        line = input()
-    except:
-        break
-    if line == "": break
-    #normalizedLine = re.sub("", "[", line)
-    normalizedLine = re.sub("    ", " -", line)
-    splitLine = normalizedLine.split(" ")
-    print(normalizedLine)
+        list.append([])
+
+    while True:
+        i = 0
+        for s in splitLine:
+            if(s != "-"):
+                list[i].insert(0, s)
+            i+=1
+        try:
+            line = input()
+        except:
+            break
+        if bool(re.match('[1-9]', line)): break
+        splitLine = line.split(" ")
+
+    line = input()
+
+    while True:
+        try:
+            line = input()
+        except:
+            break
+        splitLine = line.split(" ")
+        amount = int(splitLine[0])
+        fromStack = int(splitLine[1]) - 1
+        toStack = int(splitLine[2]) - 1
+        for i in range(0, amount):
+            list[toStack].append(list[fromStack].pop())
+    for l in list:
+        print(l[-1], end="")
+    print()
+
+def second_part():
+    list = []
+    line = input()
+    splitLine = line.split(" ")
+    for s in splitLine:
+        list.append([])
+
+    while True:
+        i = 0
+        for s in splitLine:
+            if(s != "-"):
+                list[i].insert(0, s)
+            i+=1
+        try:
+            line = input()
+        except:
+            break
+        if bool(re.match('[1-9]', line)): break
+        splitLine = line.split(" ")
+
+    line = input()
+
+    while True:
+        try:
+            line = input()
+        except:
+            break
+        splitLine = line.split(" ")
+        amount = int(splitLine[0])
+        fromStack = int(splitLine[1]) - 1
+        toStack = int(splitLine[2]) - 1
+        for i in range(-amount, 0, 1):
+            list[toStack].append(list[fromStack].pop(i))
+    for l in list:
+        print(l[-1], end="")
+    print()
+
+second_part()
