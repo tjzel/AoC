2d11e9956c0319b4ab4658cf39dd8db739bc01ce
BigPoppe <tzelawski@gmail.com>
2022-12-07 13:09:11 +0100

    day 7 done

--- 07/solution.py
+++ 07/solution.py
@@ -1,6 +1,10 @@
 import re
 
 class Node:
+    cutoff = 100000
+    totalSpace = 70000000
+    requiredFree = 30000000
+    currentFree = 0
     def __init__(self, name, parent, children, size):
         self.name = name
         self.parent = parent
@@ -12,11 +16,33 @@ class Node:
         if self.parent != None:
             self.parent.addUp(fileSize)
 
+    def sumDown(self):
+        sum = 0
+        for c in self.children:
+            sum += c.sumDown()
+        if self.size <= self.cutoff:
+            sum += self.size
+        return sum
+    
+    def findFitting(self, currentMin):
+        if self.size < currentMin and Node.currentFree + self.size >= Node.requiredFree:
+            currentMin = self.size
+        for c in self.children:
+            currentMin = c.findFitting(currentMin)
+        return currentMin
+
+    
+
 class Tree:
     def __init__(self, root):
         self.root = root
 
-sum = 0
+def first_part(fileTree):
+    print(fileTree.root.sumDown())
+
+def second_part(fileTree):
+    Node.currentFree = Node.totalSpace - rootDir.size
+    print(fileTree.root.findFitting(fileTree.root.size))
 
 rootDir = Node("/",None,[],0)
 fileTree = Tree(rootDir)
@@ -28,14 +54,7 @@ while True:
     except:
         break
     if re.match("^\$ cd", line):
-        #print(line)
         dirName = re.sub("^\$ cd ", "", line)
-        print("!!!", dirName)
-        print("available children:")
-        for c in currentDir.children:
-            print("\t", c.name)
-        #print(dir)
-        #print()
         if dirName == "/":
             currentDir = fileTree.root
         elif dirName == "..":
@@ -45,16 +64,13 @@ while True:
                 if c.name == dirName:
                     currentDir = c
                     break
-        print("entered:", currentDir.name)
-        print()
     elif re.match("^dir ", line):
         dirName = re.sub("^dir ", "", line)
-        print("adding " + dirName)
         currentDir.children.append(Node(dirName, currentDir, [], 0))
     elif re.match("^[0-9]+", line):
         fileSize = int(re.match("^[0-9]+", line)[0])
         fileName = re.sub("^[0-9]+ ", "", line)
-        #print("file size:", fileSize, "file name:", fileName)
         currentDir.addUp(fileSize)
-        #print(currentDir.size)
-    
+
+first_part(fileTree)
+second_part(fileTree)
