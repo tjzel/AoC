62be5cc207d7698bccc1dce9c5b72751de90c305
BigPoppe <tzelawski@gmail.com>
2022-12-13 23:35:18 +0100

    day 12 done

--- 12/solution.py
+++ 12/solution.py
@@ -6,9 +6,13 @@ class Square:
         self.position = position
         self.directions = []
         self.visited = 0
-    def update(self, neighbourPosition, neighbour):
-        if abs(self.key - neighbour.key) <= 1:
-            self.directions.append(neighbourPosition)
+    def update(self, neighbour):
+        if self.key - neighbour.key > -2:
+            self.directions.append(neighbour.position)
+    def visit(self):
+        self.visited = 1
+    def unvisit(self):
+        self.visited = 0
     def __lt__(self, other): return True
 
 class Map:
@@ -19,15 +23,19 @@ class Map:
         self.length = 0
         self.width = len(line)
         self.squares = []
+        self.startPoints = []
         while True:
             self.squares.append([])
             for i in range(0, self.width):
                 if line[i] == 'S':
-                    self.start = (i, self.length)
+                    self.start = (self.length, i)
                     self.squares[self.length].append(Square('a', (self.length, i)))
                 elif line[i] == 'E':
-                    self.end = (i, self.length)
+                    self.end = (self.length, i)
                     self.squares[self.length].append(Square('z', (self.length, i)))
+                elif line[i] == 'a':
+                    self.squares[self.length].append(Square('a', (self.length, i)))
+                    self.startPoints.append((self.length, i))
                 else: self.squares[self.length].append(Square(line[i], (self.length, i)))
             self.length += 1
             try: line = input()
@@ -35,44 +43,56 @@ class Map:
         for i in range (0, self.length):
             for j in range(0, self.width):
                 if j>0: 
-                    self.squares[i][j].update((i,j-1), self.squares[i][j-1])
+                    self.squares[i][j].update(self.squares[i][j-1])
                 if j<self.width-1: 
-                    self.squares[i][j].update((i,j+1), self.squares[i][j+1])
-                if i<0: 
-                    self.squares[i][j].update((i-1,j), self.squares[i-1][j])
+                    self.squares[i][j].update(self.squares[i][j+1])
+                if i>0: 
+                    self.squares[i][j].update(self.squares[i-1][j])
                 if i<self.length-1: 
-                    self.squares[i][j].update((i+1,j), self.squares[i+1][j])
-    def findShortest(self):
+                    self.squares[i][j].update(self.squares[i+1][j])
+    def findShortest(self, start):
+        self.unvisitAll()
         objectQueue = queue.PriorityQueue()
-        length = self.start[0]
-        width = self.start[1]
-        self.squares[length][width].visited = 1
+        length = start[0]
+        width = start[1]
+        self.squares[length][width].visit()
         objectQueue.put((0, self.squares[length][width]))
         while True:
+            if(objectQueue.empty()): return 0
             currentObject = objectQueue.get()
             currentPos = currentObject[1].position
             if currentPos == self.end:
                 return currentObject[0]
             length = currentObject[1].position[0]
             width = currentObject[1].position[1]
-            print(length, width)
             for d in currentObject[1].directions:
                 length = d[0]
                 width = d[1]
                 distance = currentObject[0] + 1
                 if self.squares[length][width].visited == 0:
-                    self.squares[length][width].visited = 1
+                    self.squares[length][width].visit()
                     objectQueue.put((distance, self.squares[length][width]))
+            if(objectQueue.empty()):
+                return -1
+    
+    def unvisitAll(self):
+        for sRow in self.squares:
+            for s in sRow:
+                s.unvisit()
 
-    def print(self):
-        for sq in self.squares:
-            for s in sq:
-                #print(chr(s.key+ord('a')), end=" ")
-                print(len(s.directions), end=" ")
-            print()
+def first_part():
+    map = Map()
+    map.loadMap()
+    print(map.findShortest(map.start))
 
-        
-map = Map()
-map.loadMap()
-map.print()
-print(map.findShortest())
+def second_part():
+    map = Map()
+    map.loadMap()
+    min = map.findShortest(map.start)
+    for m in map.startPoints:
+        temp = map.findShortest(m)
+        if temp != -1 and temp < min: min = temp
+    print(min)
+
+#first_part()
+second_part()
